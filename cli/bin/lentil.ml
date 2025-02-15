d384c72e00d252cd154fdc5105759a25ebd9d882
smjleo <seojun.lee@oriel.ox.ac.uk>
2025-02-15 15:20:16 -0800

    feat: cmd interface

--- cli/bin/lentil.ml
+++ cli/bin/lentil.ml
@@ -4,24 +4,26 @@ module File = Lentil.File
 
 let start_server () = print_s [%message "hi starting webserver"]
 
-let send_context () =
+let send_context cmd =
   (* testing for now *)
-  let files = Context.get () in
+  let files = Context.get cmd in
   print_s [%message (files : Context.t)]
 ;;
 
 let main cmd ~serve =
-  print_s [%message (cmd : string list)];
   match serve with
   | true -> start_server ()
-  | false -> send_context ()
+  | false ->
+    (match cmd with
+     | None -> error_s [%message "cmd empty liao"] |> ok_exn
+     | Some cmd -> send_context cmd)
 ;;
 
 let command =
   Command.basic
     ~summary:"lentil"
     (let%map_open.Command serve = flag "--serve" no_arg ~doc:"start webserver"
-     and cmd = anon (non_empty_sequence_as_list ("cmd" %: string)) in
+     and cmd = anon (maybe (non_empty_sequence_as_list ("cmd" %: string))) in
      fun () -> main cmd ~serve)
 ;;
 
--- cli/lib/context.ml
+++ cli/lib/context.ml
@@ -1,1 +1,13 @@
-  ignore cmd;
+open! Core
+
+type t =
+  { stdout : string
+  ; stderr : string
+  ; files : File.t list
+  }
+[@@deriving sexp]
+
+let get cmd =
+  print_s [%message (cmd : string list)];
+  { stdout = "test stdout"; stderr = "test stderr"; files = File.get () }
+;;
