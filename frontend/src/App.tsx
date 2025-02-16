4832956d0aeaf80ca5863285c79684451d4c4526
Edward Wibowo <wibow9770@gmail.com>
2025-02-16 02:51:27 -0500

    feat(frontend): add loading indicator

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, CSSProperties } from "react";
 import ErrorMessage from "./ErrorMessage.tsx";
 import SuggestedFix from "./SuggestedFix.tsx";
 import { Annotations, Annotation } from "./Annotations.tsx";
 import { socket } from "./socket.ts";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override: CSSProperties = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
 
 interface Response {
   summary: string;
@@ -12,33 +19,43 @@ interface Response {
 }
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
   const [response, setResponse] = useState<Response | null>(null);
 
   useEffect(() => {
+    socket.on("loading", (_: any) => {
+      setIsLoading(true);
+    });
     socket.on("response", (data: any) => {
       data = JSON.parse(data["message"]);
       setResponse(data as Response);
+      setIsLoading(false);
     });
   }, []);
+
   console.log(response);
 
   return (
     <>
       <div className="h-screen p-8 max-w-4xl m-auto mt-8 flex flex-col gap-4">
         {/* <Header /> */}
-        {response ? (
+        {!isLoading && response ? (
           <div>
             <ErrorMessage errMsg={response.summary} />
             <SuggestedFix diff={response.diff} />
             <Annotations annotations={response.annotations} />
-            <div>
-              <h2 className="text-xl font-medium my-3">
-                Here is a more detailed analysis...
-              </h2>
-              <p className="ovo-regular">{response.description}</p>
-            </div>
+            <p>{response.description}</p>
           </div>
-        ) : null}
+        ) : (
+          <ClipLoader
+            color={"#ff0000"}
+            loading={isLoading}
+            cssOverride={override}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        )}
       </div>
     </>
   );
