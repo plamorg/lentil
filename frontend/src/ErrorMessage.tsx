3694ecbb8cb1775a9d9209bb8cdec426f9f84e2e
Lucas Sta Maria <lucas@priime.dev>
2025-02-16 01:41:13 -0500

    fix: change `div` to `h1`

--- frontend/src/ErrorMessage.tsx
+++ frontend/src/ErrorMessage.tsx
@@ -1,9 +1,9 @@
 export default function ErrorMessage({ errMsg }: { errMsg: string }) {
   return (
     <>
-      <div className="text-5xl ovo-regular mt-16 mb-8">
+      <h1 className="text-5xl ovo-regular mt-16 mb-8">
         {errMsg}
-      </div>
+      </h1>
     </>
   )
 }
