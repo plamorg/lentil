edd92299af46855da72cd1b156619a5a2568cab1
smjleo <seojun.lee@oriel.ox.ac.uk>
2025-02-16 01:12:23 -0800

    feat: add command to context

--- cli/lib/context.ml
+++ cli/lib/context.ml
@@ -3,11 +3,12 @@ open! Core
 type t =
   { stdout : string
   ; stderr : string
+  ; cmd : string
   ; files : File.t list
   }
 [@@deriving sexp, yojson]
 
 let get cmd =
   let stdout, stderr = Process.run cmd in
-  { stdout; stderr; files = File.get () }
+  { stdout; stderr; cmd = String.concat ~sep:" " cmd; files = File.get () }
 ;;
